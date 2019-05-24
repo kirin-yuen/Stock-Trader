fdb5a2ee1dbb1ad851597b9aaf9f17709bb51bbc
kirin <ywl_allen@163.com>
2019-05-24 13:53:39 +0800

    stock component complete

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,11 +1,52 @@
 <template>
-  <div>
-    <h1>stocks/stock.vue</h1>
+  <div class="col-sm-6 col-md-4">
+    <div class="panel panel-success">
+      <div class="panel-heading">
+        <h3 class="panel-title">
+          {{stock.name}}
+          <small> Price: {{stock.price}} </small>
+          </h3>        
+      </div>
+      <div class="panel-body">
+        <div class="pull-left">
+          <input type="number" class="form-control" placeholder="quantity" v-model="quantity" />
+        </div>
+        <div class="pull-right">
+          <button class="btn btn-success" :disabled="buyDisabled" @click="buyStock">Buy</button>
+        </div>      
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  // 使用 props 获取父组件传过来的股票数据
+  props: ['stock'],
+  data(){
+    return {
+      quantity: 0
+    }
+  },
+  computed: {
+    // 当数量不为整数与小于 0 时禁止点击按钮
+    buyDisabled() {
+      const quantity = Number(this.quantity);
+      return quantity <= 0 || !Number.isInteger(quantity);
+    }
+  },
+  methods:{
+    // 购买股票，暂时只输出当前股票数据
+    buyStock(){
+      let order = {
+        id: this.stock.id,
+        price: this.stock.price,
+        quantity: this.quantity,
+      }
+      console.log(order);
+    }
+  }
+};
 </script>
 
 <style lang="sass" scoped>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,11 +1,28 @@
 <template>
-  <div>
-    <h1>stocks/stocks.vue</h1>
+  <div class="row">
+    <!-- 传递股票数据到子组件 -->
+    <AppStock v-for="stock in stocks" :stock="stock"></AppStock>
   </div>
 </template>
 
 <script>
-export default {};
+import AppStock from './Stock.vue';
+
+export default {
+  components: {
+    AppStock
+  },
+  data(){
+    return {
+      stocks: [
+        {id: 1, name: 'Google',price: 100},
+        {id: 2, name: 'Huawei',price: 150},
+        {id: 3, name: 'Twitter',price: 120},
+        {id: 4, name: 'Apple',price: 200},
+      ]
+    };
+  }
+};
 </script>
 
 <style scoped>
